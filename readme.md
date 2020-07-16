6a4b1a68359f61afb4045e4040ed506790bdf87b
刘晓辉 <xhliu@dfocuspace.com>
2020-07-16 10:41:53 +0800

    add readme.md

---
+++ readme.md
@@ -0,0 +1,4 @@
+# 项目说明
+1. learning typescript
+2. use api of GaoDe maps
+3. output info in console
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,55 @@
 import commander from 'commander';
 import colors from "colors";
+import axios, { AxiosResponse } from 'axios';
+
 
 const command = commander
-.version("0.1.0")
-.option("-c, --city [name]","Add city name")
-.parse(process.argv);
+    .version("0.1.0")
+    .option("-c, --city [name]", "Add city name")
+    .parse(process.argv);
 
-if(process.argv.slice(2).length === 0) {
+if (process.argv.slice(2).length === 0) {
     command.outputHelp(colors.red);
     process.exit();
 }
+
+interface IWeatherResponse {
+    status: string;
+    count: string;
+    info: string;
+    infocode: string;
+    lives: ILive[];
+}
+
+interface ILive {
+    province: string;
+    city: string;
+    adcode: string;
+    weather: string;
+    temperature: string;
+    winddirection: string;
+    windpower: string;
+    humidity: string;
+    reporttime: string;
+}
+
+const URL = "https://restapi.amap.com/v3/weather/weatherInfo";
+
+const KEY = "31de2ee01c6dcb9f1f6df56b6920f7ac";
+// const log = console.log;
+async function getWeather(city: string) {
+    try {
+        const url = `${URL}?city=${encodeURI(command.city)}&key=${KEY}`;
+        // await 只有与async 一起使用的时候才合法
+        const response = await axios.get(url);
+
+        const live = response.data.lives[0];
+        // log(colors.yellow(live.reporttime));
+        // log(colors.white(`${live.province} ${live.city}`));
+        // log(colors.green(`${live.weather} ${live.temperature} 度`));
+
+    } catch (e) {
+        // log(colors.red("天气服务出现异常:" + e));
+    }
+}
+getWeather(command.city);
